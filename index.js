5dd74caa9bc5098fb5b23068ba1cf0d0f8f4d362
aliibnemasud <mdmasudrony@gmail.com>
2023-04-07 14:38:35 +0600

    kml file added

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const path = require('path');
 const cors = require('cors');
 const port = process.env.PORT || 4000;
 
+const fs = require('fs');
+
 app.use(cors())
 
 app.get('/', async (req, res) => { 
@@ -12,6 +14,24 @@ app.get('/', async (req, res) => {
 
 // pointed the model folder
 app.use('/server', express.static(path.join(__dirname, 'model')))
+
+// pointed the kml folder
+app.use('/kml', express.static(path.join(__dirname, 'kml')))
+
+
+
+/* app.get('/kml', (req, res) => {
+  const kmlPath = path.join(__dirname, 'kml', 'cta.kml');
+  const kmlFileContent = fs.readFileSync(kmlPath, 'utf8');
+  console.log(kmlFileContent)
+  // res.setHeader('Content-Type', 'application/vnd.google-earth.kml+xml');
+  res.sendFile(kmlFileContent);
+
+}); */
+
+
+
+
 
 app.get('*', async (req, res) => { 
   res.send('No routes found!');
